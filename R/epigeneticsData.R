69a334ada3bb1555cb5b528b7ca15f884060fee4
fmueller <fmueller@mpi-inf.mpg.de>
2014-09-04 11:20:03 +0200

    ...

--- R/epigeneticsData.R
+++ R/epigeneticsData.R
@@ -7,9 +7,9 @@
 #' @export
 #' @aliases getRegionSet
 #' @examples 
-#' regs <- getRegionSet("deep_chip_ctrl_regions")
-getRegionSet <- function(id,assembly="hg19"){
-	reg.fname <- system.file(file.path("extdata", paste(id,assembly,"bed",sep="."), package = "muRtools"))
+#' regs <- getRegionSet("deep_chip_ctrl_regions","hg19")
+getRegionSet <- function(id,assembly){
+	reg.fname <- system.file(file.path("extdata", paste(id,assembly,"bed",sep=".")), package = "muRtools")
 	res <- NULL
 	if (!file.exists(reg.fname)){
 		stop(paste0("Error in retrieving region set. Could not open region file: ",reg.fname))
@@ -22,7 +22,8 @@ getRegionSet <- function(id,assembly="hg19"){
 
 #' bed2GRanges
 #' 
-#' loads a bed file and converts it to a GRanges object
+#' loads a bed file and converts it to a GRanges object. Note that bed files are 0-based, right-exclusive by definition
+#' The output of this function will be 1-based, right inclusive as defined by GRanges.
 #' @param id region set id
 #' @param assembly genome assembly
 #' @return a GRanges object containing the region set
@@ -64,10 +65,13 @@ bed2GRanges <- function(fname,assembly=NA){
 	bed.col.inds <- 1:min(length(BED.COLUMNS),ncol(tbl))
 	colnames(tbl)[bed.col.inds] <- BED.COLUMNS[bed.col.inds]
 
+	sns <- tbl$chrom
+	if (!is.na(assembly)) {
+		sns <- factor(sns,levels=names(sls))
+	}
 	res <- GRanges(
-		seqnames=tbl$chrom,
-		ranges=IRanges(tbl$start,end=tbl$end),
-		elementMetadata=cpg.meth.table[rownames(c.annot.table),]
+		seqnames=sns,
+		ranges=IRanges(tbl$start + 1,end=tbl$end) #0-based, right exclusive --> 1-based, right inclusive
 	)
 	if (is.element("strand",colnames(tbl))) {
 		strand(res) <- tbl$strand
@@ -85,7 +89,7 @@ bed2GRanges <- function(fname,assembly=NA){
 		names(res) <- tbl$id
 	}
 	if (!is.na(assembly)) {
-		seqlengths(res) <- sls[names(seqlengths(res))]
+		seqlengths(res) <- sls
 	}
 	return(res)
 }
