e5d1fc8f0f988d956a55529de4d9c8804416fba8
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-06-06 10:07:39 -0700

    deps

---
+++ R/muRtools.R
@@ -0,0 +1,14 @@
+#' muRtools: Mueller's R tools
+#'
+#' Fabian's custom plotting functions and utilities
+#'
+#' @import muLogR
+#' @import gplots
+#' @import plotrix
+#' @import ggplot2
+#' @import data.table
+#' @import GenomicRanges
+#' @import GenomeInfoDb
+#' @docType package
+#' @name muRtools
+NULL
